a8350b7e0d8fb2641fddb27b24f56cf58875c04e
callmekurisu <callmekurisu@gmail.com>
2016-12-27 20:46:44 -1000

    completed header api app (^_^)

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const url = require('url')
 var moment = require('moment')
 var ip = require('ip')
 var os = require('os')
+var navigator = require('navigator')
 var fs = require("fs")
 var accepts = require('accepts')
 var html = (fs.readFileSync('header.html').toString())
@@ -23,17 +24,22 @@ app.set('port', (process.env.PORT || 5000));
     app.get('/api',function (req, res) {
       var ipAddy = ip.address()
       var os =  process.platform
-      var lang = JSON.stringify(request.get("https://callmekurisu-headerparse.herokuapp.com"))
-      res.send('{"ip: '+ipAddy+", "+"os: "+os+", "+"lang: "+lang+'"}')
+      var header = req.headers["accept-language"]
+      var box = [];
+      for(i=0;i<5;i++){
+	box.push(header[i])
+      }
+      var lang = box.join("")
+      res.send('{"ip": '+'"'+ipAddy+'"'+", "+'"os": '+'"'+os+'"'+", "+'"lang": '+'"'+lang+'"}')
    
 })
 
-   
+  
     app.listen(process.env.PORT, '0.0.0.0', function(err) {
   console.log("Started listening on %s", app.url);
 });
   
-  /*comment out for running on localhost
+  /*uncomment out for running on localhost
     app.listen(5000, function(err) {
   console.log("Started listening on port 5000...");
 });*/
